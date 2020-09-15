9e0324dac3d44a35853a8668706fbcf3d9d4b219
piratecaveman <iamrobox001@tutanota.com>
2020-09-15 12:45:37 +0530

    added alacritty

--- home/leshen/.environ/export.d/exports.sh
+++ home/leshen/.environ/export.d/exports.sh
@@ -5,7 +5,8 @@ export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export VDPAU_DRIVER='nvidia'
 export AWT_TOOLKIT='MToolkit'
 export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeeli -Dswing.aatext=true'
-
+export WINEDEFAULT=$HOME'/winestore/default-prefix'
+export WINEPREFIX=$WINEDEFAULT
 
 if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
 	PATH="${PATH}:${HOME}/.local/bin"
