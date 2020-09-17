d0c4d0d2dabf586682442e95d2ea0f8f953920fc
piratecaveman <iamrobox001@tutanota.com>
2020-09-17 16:44:21 +0530

    polybar mod

--- home/leshen/.config/polybar/launch.sh
+++ home/leshen/.config/polybar/launch.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
@@ -7,5 +9,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -r topbar --log=warning 2>/tmp/ploybar.log &
-
+polybar -r main --log=info -c ~/.config/polybar/config.ini 2>/tmp/polybar.log &
--- home/leshen/.environ/export.d/exports.sh
+++ home/leshen/.environ/export.d/exports.sh
@@ -4,9 +4,10 @@ export QT_QPA_PLATFORMTHEME='qt5ct'
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export VDPAU_DRIVER='nvidia'
 export AWT_TOOLKIT='MToolkit'
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeeli -Dswing.aatext=true'
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 export WINEDEFAULT=$HOME'/winestore/default-prefix'
 export WINEPREFIX=$WINEDEFAULT
+export TDESKTOP_DISABLE_TRAY_COUNTER="True"
 
 if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
 	PATH="${PATH}:${HOME}/.local/bin"
